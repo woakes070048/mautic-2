378b6f4a55e91b75bd912d538c1fbad87ae346cb
Noel Light-Hilary <noel@noellh.com>
2022-05-28 18:57:19 +0100

    INFRA-61 – fix SSL proxy redirect loop

--- secrets_entrypoint.sh
+++ secrets_entrypoint.sh
@@ -12,5 +12,20 @@ fi
 # Load the S3 secrets file contents into the environment variables
 export $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/secrets - | grep -v '^#' | xargs)
 
+# Fix redirect loop behind ALB. https://forum.mautic.org/t/mautic-redirect-loop/16990/3
+INDEX_FILE="/var/www/html/index.php"
+NEW_LINE="\$_SERVER[\"HTTPS\"] = \"on\";\n"
+if test -f "$INDEX_FILE"; then
+  if grep -q "$NEW_LINE" "$INDEX_FILE"; then
+    echo "SSL modification already in place"
+  else
+    # Existing line regex based on https://github.com/mautic/mautic/blob/4.x/index.php
+    sed -i -e 's/(<\?php)/$1\n\$_SERVER["HTTPS"] = "on";/g' "$INDEX_FILE"
+    echo "SSL modification applied"
+  fi
+else
+  echo "No index.php – no SSL modification"
+fi
+
 # Call the normal Mautic entrypoint script then the e.g. start Apache CMD (argument to entrypoint).
 /entrypoint.sh "$@"
