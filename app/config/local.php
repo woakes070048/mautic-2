6dcf17d54a7c98c7249b3d674970f4783aa9384d
Noel Light-Hilary <noel@noellh.com>
2023-09-01 18:41:22 +0100

    INFRA-107 – check in local changes used for in-browser install

---
+++ app/config/local.php
@@ -0,0 +1,10 @@
+<?php
+$parameters = array (
+    'db_driver' => 'pdo_mysql',
+    'install_source' => 'Docker',
+    'db_port' => '3306',
+    'db_host' => 'mauticdb',
+    'db_name' => 'mautic',
+    'db_user' => 'root',
+    'db_password' => 'mysecret',
+);
--- secrets_entrypoint.sh
+++ secrets_entrypoint.sh
@@ -12,10 +12,6 @@
 ## Load the S3 secrets file contents into the environment variables
 #export $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/secrets - | grep -v '^#' | xargs)
 
-# Call our copy of the Mautic entrypoint script then the e.g. start Apache CMD
-# (argument to entrypoint).
-# The one change in our entrypoint vs. that upstream as of 13/4/23 is the addition
-# of a modified index.php which we copy at startup, so that we can change it after
-# the normal auto install process completes. The purpose of that modified index.php
-# is to fix infinite redirect loops when using an ALB with SSL.
-/entrypoint.sh "$@"
+echo "Starting Apache..."
+# No args for now. Our base php:8.0 already turns on the `mautic` vhost.
+apache2-foreground
